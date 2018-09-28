179daf2f3256e39083f19551f72746ee7242d76e
Phil Smith <phil@amillionmonkeys.co.uk>
2018-09-28 10:35:00 +0100

    lots of changes

---
+++ wordpress/wp-content/themes/dmd-theme/header.php
@@ -0,0 +1,62 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<title><?php wp_title(''); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<!--[if IE]>
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<![endif]-->
+
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php wp_head(); ?>
+
+
+	</head>
+
+	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+		<header class="c-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+			<div class="c-header__container">
+				<div class="c-header__logo" itemscope itemtype="http://schema.org/Organization">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="DMD Pathfinders"> <div class="c-menu-toggle" id="menuToggle">
+			<span class="line"></span>
+			<span class="line"></span>
+			<span class="line"></span>
+		</div>
+				</div>
+				<div class="c-header__extras">
+					<nav class="c-main__nav" itemscope itemtype="http://schema.org/SiteNavigationElement">
+						<?php wp_nav_menu(array(
+                                 'container' => false,                           // remove nav container
+                                 'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                                 'menu' => __('The Main Menu', 'bonestheme'),  // nav name
+                                 'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                                 'theme_location' => 'main-nav',                 // where it's located in the theme
+                                 'before' => '',                                 // before the menu
+                                   'after' => '',                                  // after the menu
+                                   'link_before' => '',                            // before each link
+                                   'link_after' => '',                             // after each link
+                                   'depth' => 0,                                   // limit the depth of the nav
+                                 'fallback_cb' => ''                             // fallback function (if there is one)
+                        )); ?>
+
+					</nav>
+					<?php get_search_form(); ?>
+					
+				</div>
+			</div>
+
+		</header>
---
+++ wordpress/wp-content/themes/dmd-theme/searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" class="c-search" action="<?php echo home_url('/'); ?>">
+    <div>
+        <label for="s" class="visuallyhidden">Search for:</label>
+        <input type="search" class="c-search__input" placeholder="Search DMD Pathfinders" name="s" value="" />
+
+        <button type="submit" class="c-search__submit" >Submit</button>
+    </div>
+</form>
