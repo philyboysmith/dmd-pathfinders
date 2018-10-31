bcb6d030330c2ed0dd1b57b5425d8203644302f0
Phil Smith <phil@amillionmonkeys.co.uk>
2018-10-31 08:30:58 +0000

    add theme

---
+++ wordpress/wp-content/themes/dmd-theme/footer.php
@@ -0,0 +1,41 @@
+<footer class="c-footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+    <div class="c-footer__container padding-bottom--l">
+        <?php wp_nav_menu(array(
+        'container' => false,
+        'menu' => __('Footer Links', 'bonestheme'),
+        'menu_class' => 'nav footer-nav',
+        'theme_location' => 'footer-links'
+        )); ?>
+		</ul>
+    </div>
+    <div class="bg--neutral--darkest padding-top--l padding-bottom--l">
+        <div class="c-footer__container l-container--flex">
+            <div class="c-footer__meta padding-top--m " style="flex:1">
+            
+                <p>&copy; <?php echo date('Y')?> DMD Pathfinders Registered Charity 1155884<br />
+                Mercury House, Ham Rd, Shoreham-by-Sea BN43 6EW</p>
+            </div>
+            <div class="c-footer__dmd text-center">
+                <p>Supported by:<br />
+                <a href="https://www.duchenneuk.org/" target="_blank"><img src="http://hercules.duchenneuk.org/assets/img/duchenne_logo.svg" alt="duchenne uk"/></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+		<?php wp_footer(); ?>
+
+<script>
+
+$(document).ready(function() {
+if ($('.switch').length>0) {
+    $('.switch').click();
+    console.log('Testing console');
+}});
+
+</script>
+	</body>
+
+</html> <!-- end of site. what a ride! -->
---
+++ wordpress/wp-content/themes/dmd-theme/page.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+<div class="space-bottom--l">
+    <div class="c-strip">
+        <div class="l-container">
+			<?php custom_breadcrumbs(); ?>
+			<h1 itemprop="headline"><?php the_field('hero_title'); ?></h1>
+            <p><?php the_field('hero_body'); ?></p>
+        </div>
+    </div>
+</div>
+
+<div class="l-container  l-container--flex padding-bottom--xl">
+	<div class="l-container__sidebar">
+		<?php get_template_part('subnav'); ?>
+	</div>
+    <main class="l-container__main">
+		<section itemprop="articleBody">
+
+			<?php
+                // the content (pretty self explanatory huh)
+                the_content();
+
+                wp_link_pages(array(
+                    'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'bonestheme') . '</span>',
+                    'after'       => '</div>',
+                    'link_before' => '<span>',
+                    'link_after'  => '</span>',
+                ));
+            ?>
+		</section>    
+	</main>
+
+    <div class="l-container__sidebar">
+    <?php 
+
+// vars
+$pages = get_field('related_pages');
+
+
+
+// check
+if ($pages): ?>
+
+	<?php foreach ($pages as $page): ?>
+
+<a class="c-card" href="<?php echo get_permalink($page->ID); ?>">
+
+<?php echo get_the_post_thumbnail($page->ID, 'small'); ?>
+<div class="c-card__content">
+    <h4><?php echo $page->post_title; ?></h4>
+    <p><?php echo $page->post_excerpt; ?></p>
+    <p>Read more</p>
+</div>
+</a>
+<?php endforeach; ?>
+<?php endif; ?>
+</div>
+    
+</div>
+
+</article>
+
+<?php endwhile; endif; ?>
+
+
+
+<?php get_footer(); ?>
